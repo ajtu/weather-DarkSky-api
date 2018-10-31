0da1d70d421b87681e25537b0b661e18e15657af
Alvin <alvintu@Alvins-MacBook-Pro.local>
2018-10-31 09:45:04 -0400

    Initial Commit

---
+++ weather-DarkSky-api/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather-DarkSky-api
+//
+//  Created by Alvin on 10/31/18.
+//  Copyright © 2018 AlvinTu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ weather-DarkSky-apiTests/weather_DarkSky_apiTests.swift
@@ -0,0 +1,34 @@
+//
+//  weather_DarkSky_apiTests.swift
+//  weather-DarkSky-apiTests
+//
+//  Created by Alvin on 10/31/18.
+//  Copyright © 2018 AlvinTu. All rights reserved.
+//
+
+import XCTest
+@testable import weather_DarkSky_api
+
+class weather_DarkSky_apiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
